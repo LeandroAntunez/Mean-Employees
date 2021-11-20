50028c73f94f36e16bcf96f80c1c205d099ca548
Leandro Antunez <leandro_antunez@outlook.com>
2021-11-20 15:06:06 -0300

    Mongoose and Morgan dependencies installed.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const { request } = require('https');
+const morgan = require('morgan');
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+
 
 // Routes
 
 
 // Starting the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port 3000')
+    console.log('Server on port', app.get('port'))
 });
